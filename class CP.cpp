309ebf911e97b662b95f676e7364e912b0395e43
Umer Mehmood <476061@gmail.com>
2021-12-05 18:41:29 +0500

    Operator overloading. global istrean and ostream functions to mitigate the use of friend method

--- class CP.cpp
+++ class CP.cpp
@@ -10,6 +10,14 @@ class Fraction
         num = den;
         den = temp;
     }
+	/**
+	 * @brief simplifies the fraction
+	 * for instance 1/2 -> 1/2 and 2/4 -> 1/2
+	 * @return void
+	 * @param void
+	 * @pre the fraction must be initialized
+	 *  
+	 */
     void normalize(){
         for (size_t i = 2; i <= num && i <= den; i++)
         {
@@ -23,20 +31,40 @@ class Fraction
     }
 
 public:
-
+	/**
+	 * @brief Construct a new Fraction object
+	 */
 	Fraction() {
-		num = 0;
+		num = 1;
 		den = 1;
 	}
+	/**
+	 * @brief Construct a new Fraction object
+	 * 
+	 * @param n numerator
+	 */
 	Fraction(int n){
 		num = n;
 		den = 1;
 
 	}
+	/**
+	 * @brief Construct a new Fraction object
+	 * 
+	 * @param n numerator
+	 * @param d denominator
+	 */
 	Fraction(int n, int d) {
 		num = n;
 		den = d > 0 ? d : 1;
 	}
+	// copy constructor
+	Fraction(const Fraction& f) {
+		num = f.num;
+		den = f.den;
+		cout << "copy constructor" << endl;
+	}
+	// 
 	Fraction operator +(Fraction f2)
 	{
 		Fraction f3;
@@ -75,7 +103,7 @@ public:
         return f3;
 
 	}
-	Fraction operator ++(){
+	Fraction& operator ++(){
 		Fraction fraction(1);
 		*this = *this + fraction;
 		return *this;
@@ -99,22 +127,133 @@ public:
 		cout << num << "\\" << den << endl;
 	}
 
-	friend ostream& operator<<(ostream& out,const Fraction& fraction){
-		out << fraction.num << "\\" << fraction.den;
-		return out;
-	}
-	friend istream& operator>>(istream& in,Fraction& fraction){
-		cout << "Enter Numerator: ";
-		in >> fraction.num;
-		cout << "Enter Denominator: ";
-		in >> fraction.den;
-		return in;
-	};
+	// 02-12-2021
+
+	// friend ostream& operator<<(ostream& out,const Fraction& fraction){
+	// 	out << fraction.num << "\\" << fraction.den;
+	// 	return out;
+	// }
+	// friend istream& operator>>(istream& in,Fraction& fraction){
+	// 	cout << "Enter Numerator: ";
+	// 	in >> fraction.num;
+	// 	cout << "Enter Denominator: ";
+	// 	in >> fraction.den;
+	// 	return in;
+	// };
 
     void simplify(){
         normalize();
     }
+
+	int getNumerator(){
+		return num;
+	}
+
+	int getDenominator(){
+		return den;
+	}
+
+	void setNumerator(int val){
+		num = val;
+	}
+
+	void setDenominator(int val){
+		den = val;
+	}
 };
+
+ostream& operator <<(ostream& out,Fraction& f){
+	out << f.getNumerator() << "\\" << f.getDenominator();
+	return out;
+}
+istream& operator>>(istream& in,Fraction& fraction){
+	int temp;
+	cout << "Enter Numerator: ";
+	in >> temp;
+	fraction.setNumerator(temp);
+	cout << "Enter Denominator: ";
+	in >> temp;
+	fraction.setDenominator(temp);
+	return in;
+};
+
+// create a class for queue
+class Queue
+{
+	int *arr;
+	int front;
+	int rear;
+	int size;
+	int capacity;
+
+	void resize(){
+		int *temp = new int[capacity * 2];
+		for (size_t i = 0; i < capacity; i++)
+		{
+			temp[i] = arr[i];
+		}
+		delete[] arr;
+		arr = temp;
+		capacity *= 2;
+	}
+public:
+	Queue(int cap) {
+		capacity = cap;
+		arr = new int[capacity];
+		front = 0;
+		rear = 0;
+		size = 0;
+	}
+	~Queue() {
+		delete[] arr;
+	}
+	void enqueue(int val) {
+		if (size == capacity)
+		{
+			resize();
+		}
+		arr[rear] = val;
+		rear = (rear + 1) % capacity;
+		size++;
+	}
+	int dequeue() {
+		if (size == 0)
+		{
+			return -1;
+		}
+		int val = arr[front];
+		front = (front + 1) % capacity;
+		size--;
+		return val;
+	}
+	int frontValue() {
+		if (size == 0)
+		{
+			return -1;
+		}
+		return arr[front];
+	}
+	int rearValue() {
+		if (size == 0)
+		{
+			return -1;
+		}
+		return arr[rear];
+	}
+	int sizeOfQueue() {
+		return size;
+	}
+	bool isEmpty() {
+		return size == 0;
+	}
+	bool isFull() {
+		return size == capacity;
+	}
+};
+
+// create a class for stack
+
+
 int main() {
 	Fraction f1(5, 3); //f1
 	Fraction f2(3); //f2=5/4	
@@ -124,15 +263,17 @@ int main() {
     // f2.simplify();
     // f2.display();
 	Fraction f3;
-
 	f3 = f1 * f2;
 	f3.display();
     // f2.display();
 	f3.display();
-	
-	cout << "POST FIX:" << ++f1 << endl;
-	cout << "PRE FIX:" << f1++ << endl;
+	cin >> f1;
+	cout << "POST FIX:";
 	cout << f1;
+	cout << "PRE FIX:" << ++++f1 << endl;
+	cout << f1;
+	// cout << ++++1;
+	// cout << 1++++;
 
 
 	return 0;
